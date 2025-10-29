423df8eeaeac93282bee90ad4de5c14e80f11f76
Waqhar <Waqhar.email@indivillage.co>
2025-10-29 15:44:37 +0530

    Added a game Object variable focal point and access it to control player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine; 
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-     private Rigidbody playerRB;
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
     public float speed = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerRB = GetComponent<Rigidbody>();
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
     }
 
     // Update is called once per frame
     void Update()
     {
-      float forwardInput = Input.GetAxis("Vertical");
-      playerRB.AddForce(Vector3.forward * speed * forwardInput); 
+        float forwardInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
     }
 }
